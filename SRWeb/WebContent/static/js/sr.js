121826e3a80057ea047bfeaf6703431265fed4b7
spiropulo <spiropulo@yahoo.com>
2014-08-22 10:15:17 -0700

    formatting.

--- SRWeb/WebContent/static/js/sr.js
+++ SRWeb/WebContent/static/js/sr.js
@@ -2,13 +2,13 @@ function handleData(data) {
 	document.getElementById("rate_" + currentId).innerHTML = data;
 }
 
-function vote(id, rate){
+function vote(id, rate, ctx){
 	var voteData = "id=" + id + "&rate=" + rate;
 	try{
 	    $.ajax({
 	        type: 'GET',
 	        contentType: 'text/plain',
-	        url: '/w/vote',
+	        url: ctx + '/vote',
 	        dataType: "text",
 	        data: voteData,
 	        success: handleData,
