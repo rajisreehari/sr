451189034531c053fc32efba04d20dbe31a4ba90
spiropulo <spiropulo@yahoo.com>
2014-09-29 16:02:37 -0700

    asfsdfasdf

--- alison/includes/left_nav.php
+++
@@ -1,9 +0,0 @@
-
-<div class="left_nav_container">
-	<div class="left_nav_link"><a href="my_philosophy_approach.php">My Philosophy-Approach</a></div>
-	<div class="left_nav_link"><a href="my_credentials.php">My Credentials</a></div>
-	<div class="left_nav_link"><a href="my_services_support.php">My Services/Support</a></div>
-	<div class="left_nav_link"><a href="my_background.php">My Background</a></div>
-	<div class="left_nav_link"><a href="contact.php">Contact</a></div>
-	<div class="left_nav_link"><a href="special_links.php">Special Links</a></div>
-</div>
