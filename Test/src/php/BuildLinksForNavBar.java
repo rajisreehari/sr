66a4704b916a7d56fe2378e42621b6cb4c210cd3
spiropulo <spiropulo@yahoo.com>
2014-09-29 15:12:16 -0700

    asdfsadfasdf

--- Test/src/php/BuildLinksForNavBar.java
+++
@@ -1,9 +0,0 @@
-package php;
-
-import java.io.IOException;
-import java.util.List;
-
-public class BuildLinksForNavBar extends DadPhp{
-
-
-}
--- Test/src/php/test/index.php
+++
@@ -1,8 +0,0 @@
-<?php include $_SERVER['DOCUMENT_ROOT']."/jorge/common/header.php"; ?>
-<div class="row">
-<h1> <br><small>(Inspired By )</small></h1>
-</div>
-<div class="row">
-<a href="/jorge/painting/test/jorge.php"><IMG src="/jorge/painting/test/jorge.jpg" class="img-thumbnail lonelyImage indexImage370"></a>
-</div>
-<?php include $_SERVER['DOCUMENT_ROOT']."/jorge/common/footer.php"; ?>
